3f4bf31f2926a8c918ffc11203dc0168cb5d2038
carla-cornetta <157768521+carla-cornetta@users.noreply.github.com>
2024-01-27 23:23:48 -0300

    Delete Trabajo 29.py

--- Trabajo 29.py
+++
@@ -1,8 +0,0 @@
-cadena_ingresada = input("Ingrese la cadena:")
-
-if cadena_ingresada == "ESPATIFILO":
-    print("Si - ¡El Espatifilo! es la mejor planta de todos los tiempos")
-elif cadena_ingresada == "espatifilo":
-    print("No, ¡quiero un gran Espatifilo!")
-else:
-    print("¡Espatifilo!, ¡No{}".format(cadena_ingresada))
